3bfa95a6be21e897003831eb97b0aa3a89f24318
Juan98D <borrero@018000@gmail.com>
2022-07-05 22:41:23 -0500

    Se agregan las cards para las peliculas destacadas, se agrega el nombre e imagen de cada pelicula, queda pendiente el boton de ver más y la descripción de cada pelicula

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const BASE = 'https://api.themoviedb.org/3'
+const BASE = 'https://api.themoviedb.org/3';
+const BASE_IMAGE = 'https://image.tmdb.org/t/p/w200';
 
 async function getTrendingMoviesPreview () {
     const res = await fetch(BASE + '/trending/movie/day?api_key=' + API_KEY);
@@ -9,12 +10,15 @@ async function getTrendingMoviesPreview () {
     
     movies.forEach(element => {
         //Creación de elements
-        const destacados = document.getElementById('destacados')
+        const destacados = document.getElementById('destacados');
         const movieConteiner = document.createElement('article');
         const movieCard = document.createElement('div');
         const movieTitle = document.createElement('h3');
         const img = document.createElement('img');
         const parrafo = document.createElement('p');
+
+        //Creación de los textos que agregaremos en cada element
+        const cardTitle = document.createTextNode(`${element.original_title}`);
 
         //Asignación de clases
         movieConteiner.className = 'movie-container--card';
@@ -23,12 +27,19 @@ async function getTrendingMoviesPreview () {
         img.className = 'img-card';
         parrafo.className = 'cards-parrafo';
 
+        //Agregar Atributos a los elementos
+        img.alt = `Poster de la pelicula ${element.original_title}`
+        img.src = `${BASE_IMAGE}/${element.poster_path}`
+
         //Agregar al HTML
         destacados.appendChild(movieConteiner);
         movieConteiner.appendChild(movieCard);
         movieCard.appendChild(movieTitle);
         movieCard.appendChild(img);
         movieCard.appendChild(parrafo);
+
+        //Agregamos los textos a los elementos
+        movieTitle.appendChild(cardTitle);
 
     });
 }
