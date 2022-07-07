270ac8c6a8b708827a810c40d2c6faa86c299a97
Juan98D <borrero@018000@gmail.com>
2022-07-06 22:51:46 -0500

    Se agrega la sipnosis en las cards de oeliculas, queda aregar el boton de ver más en las cards de peliculas, el cual nos llevara a una pestaña más detallada de la pelicula, adicionalmente se crea la barra de busqueda de categoria, lo proxiomo es capturar el texto que se escriba para poder buscar las categorias

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 const BASE = 'https://api.themoviedb.org/3';
 const BASE_IMAGE = 'https://image.tmdb.org/t/p/w200';
 
+
+function ellipsis_box(elemento, max_chars){
+	limite_text = elemento;
+	if (limite_text.length > max_chars) {
+	    limite = limite_text.substr(0, max_chars)+" ...";
+	    elemento += limite;
+        return limite;
+
+	}
+}
+
 async function getTrendingMoviesPreview () {
     const res = await fetch(BASE + '/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json();
@@ -9,6 +20,16 @@ async function getTrendingMoviesPreview () {
     console.log(movies)
     
     movies.forEach(element => {
+
+        let x = element.overview;
+        let text = ''
+            
+        if (x.length < 200){
+            text = element.overview
+        }else{
+            text = (ellipsis_box(x, 200));
+        }
+        
         //Creación de elements
         const destacados = document.getElementById('destacados');
         const movieConteiner = document.createElement('article');
@@ -16,9 +37,12 @@ async function getTrendingMoviesPreview () {
         const movieTitle = document.createElement('h3');
         const img = document.createElement('img');
         const parrafo = document.createElement('p');
+        const btnVerMas = document.createElement('button')
 
         //Creación de los textos que agregaremos en cada element
         const cardTitle = document.createTextNode(`${element.original_title}`);
+        const description = document.createTextNode(`Sinopsis: ${text}`)
+        const verMas = document.createTextNode('ver más');
 
         //Asignación de clases
         movieConteiner.className = 'movie-container--card';
@@ -26,10 +50,11 @@ async function getTrendingMoviesPreview () {
         movieTitle.className = 'cards-title';
         img.className = 'img-card';
         parrafo.className = 'cards-parrafo';
+        btnVerMas.className = 'boton-ver_mas';
 
         //Agregar Atributos a los elementos
-        img.alt = `Poster de la pelicula ${element.original_title}`
-        img.src = `${BASE_IMAGE}/${element.poster_path}`
+        img.alt = `Poster de la pelicula ${element.original_title}`;
+        img.src = `${BASE_IMAGE}/${element.poster_path}`;
 
         //Agregar al HTML
         destacados.appendChild(movieConteiner);
@@ -40,6 +65,8 @@ async function getTrendingMoviesPreview () {
 
         //Agregamos los textos a los elementos
         movieTitle.appendChild(cardTitle);
+        parrafo.appendChild(description);
+        btnVerMas.appendChild(verMas);
 
     });
 }
